5d8cb1e0f3b3619c8d4e1260f0019fd75d96c7c9
drewbob <122831351+drewbobO1@users.noreply.github.com>
2025-03-19 13:52:08 -0500

    feat: Add 04 work

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -9,10 +9,57 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+
+const cubesEtcGroup = new THREE.Group();
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ffff })
+);
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+
+cube1.position.x = -2;
+cube2.position.set(0, 2, 0);
+cube3.position.set(2, 1, 0);
+
+cubesEtcGroup.add(cube1);
+cubesEtcGroup.add(cube2);
+cubesEtcGroup.add(cube3);
+
+cubesEtcGroup.position.y = -2;
+cubesEtcGroup.scale.y = 2;
+cubesEtcGroup.rotation.y = 1;
+
+scene.add(cubesEtcGroup);
+
+
+// Set mesh (object) position
+// mesh.position.set(0, -2, -4);
+
+// Change mesh (object) scale
+// mesh.scale.x = 10;
+// mesh.scale.y = .5;
+// mesh.scale.z = 2;
+
+// mesh.scale.set(5, .5, 1);
+
+// Rotating mesh
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+// mesh.rotation.z = Math.PI * 0.25;
+
+// Axes helper (visualizer)
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -26,7 +73,12 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 5;
+
+// camera.lookAt(mesh.position);
+
 scene.add(camera)
 
 /**
